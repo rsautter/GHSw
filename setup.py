d7afaa85178d31e61008d76f9aca99c97a5de33f
Rubens <rubens.sautter@gmail.com>
2020-10-11 11:27:00 -0300

    Setup

--- setup.py
+++ setup.py
@@ -8,7 +8,5 @@ setup(
     author_email='rubens.sautter@gmail.com',
     keywords='histogram GHS statistic',
     description=u'Geometric Histogram of Separation',
-    packages=['ghsw'],
-    package_dir={'': 'ghsw'},
-    packages=find_packages(where='ghsw')
+    package_dir={'': 'ghsw'}
 )
